49c391515a9640a644909f8c35fd8395a6fac6c3
Nick Larson <nick.larson@gmail.com>
2011-09-28 21:18:25 -0500

    first commit

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,5 @@
+class RequestsController < ApplicationController
+
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curlosaurus::Application.config.secret_token = 'cba93d591d079d266d4dff2a44b0d46d8f7c7f0b09eab28528ff12359737e12ba5037fe65b08bb005d600ecffe5a641cd1cd17f17aff289a8c3a8cf0eb91ee6a'
