fdf8b4f8325bdf35ebd2088939770171e3e78260
Nick Larson <nick.larson@gmail.com>
2011-10-03 12:09:40 -0500

    fixed blur/focus font color not changing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,17 +5,17 @@ function label_togglerizer(el) {
   $(el).each(function() {
     var self = $(this), title = self.attr('title');
 
-    self.val(title).css('color', '#bbb');
+    self.val(title).addClass('blurred');
 
     self.blur(function() {
       if (self.val() == '') {
-        self.val(title).removeClass('focused');
+        self.val(title).addClass('blurred');
       }
     });
 
     self.focus(function() {
       if (self.val() === title) {
-        self.val('').addClass('focused');
+        self.val('').removeClass('blurred');
       }
     });
   });
